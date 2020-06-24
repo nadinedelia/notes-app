0cb182afe0b218f1f070ddc34c50308222a32ddf
Rachael <rachaelewins@hotmail.com>
2020-06-24 17:43:31 +0100

    changed output on a fail

--- spec/assert.js
+++ spec/assert.js
@@ -1,7 +1,7 @@
 var assert = {
   isTrue: function(assertionToCheck, number) {
     if (!assertionToCheck) {
-      throw new Error("Nah, mate. Sorry, it's " + assertionToCheck + ". \n Test not working. \n \n" + number);
+      console.log("Nah, mate. Sorry, it's " + assertionToCheck + ". \n Test not working. \n \n" + number);
     }
     else {
       console.log('YEAH BABY IT WORKS!!', number)
