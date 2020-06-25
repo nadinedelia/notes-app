ba4b43aea867ce9b7e81f0aea3315ed71908f3b3
J Perrin <perrinjack96@gmail.com>
2020-06-25 15:44:15 +0100

    displays on page

--- note-controller.js
+++ note-controller.js
@@ -1,24 +1,25 @@
-
-(function(exports) {
+(function (exports) {
   function NoteController(noteList) {
-    this.noteList = new NoteList
-    this.noteList.addNote('Beer n Wine n BBQ')
-    console.log(this.noteList.notesArray)
-    this.noteListView = new NoteListView(noteList)
-  };
+    this.noteList = new NoteList();
+    this.noteList.addNote('Beer n Wine n BBQ');
+    this.noteList.addNote('Beer BBQ');
 
-  NoteController.prototype.displayHtml = function() {
-    document.getElementById("app").innerHTML = this.noteListView.displayView();
+    console.log(this.noteList.notesArray);
+    this.noteListView = new NoteListView(this.noteList);
+  }
+
+  NoteController.prototype.displayHtml = function () {
+    document.getElementById('app').innerHTML = this.noteListView.displayView();
   };
 
   exports.NoteController = NoteController;
 })(this);
 
-window.addEventListener(‘DOMContentLoaded’, function (event) {
-  noteList = new NoteList
-  noteList.addNote('Beer n Wine n BBQ')
-  noteListView = new NoteListView(noteList)
-  console.log(noteListView)
-  document.getElementById("app").innerHTML = noteListView.displayView();
-
+window.addEventListener(`DOMContentLoaded`, function (event) {
+  var noteList = new NoteList();
+  var controller = new NoteController(noteList);
+  document.getElementById(
+    'app'
+  ).innerHTML = controller.noteListView.displayView();
+  console.log(controller.noteList.getNotes());
 });
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,17 +1,20 @@
 // Takes a note list model upon instantiation
 
-(function(exports) {
+(function (exports) {
   function NoteListView(noteList) {
     this.noteList = noteList;
-  };
+  }
 
-  NoteListView.prototype.displayView = function() {
-      var notearray = this.noteList.getNotes();
-      output = '<ul>'
-      notearray.forEach (function(note) { output += `<li><div>${note.getText().substring(0,20)}</div></li>` })
-      output += "</ul>"
-      console.log(output)
-      return output
+  NoteListView.prototype.displayView = function () {
+    console.log(this.noteList.getNotes());
+    var notearray = this.noteList.getNotes();
+    output = '<ul>';
+    notearray.forEach(function (note) {
+      output += `<li><div>${note.getText().substring(0, 20)}</div></li>`;
+    });
+    output += '</ul>';
+    console.log(output);
+    return output;
   };
 
   exports.NoteListView = NoteListView;
