40069adee9bbe9bc47ac6b2b087a2b10fbfdfdfd
Tristan Langford <tristanlangford@hotmail.com>
2020-06-26 15:48:19 +0100

    added back button

--- note-controller.js
+++ note-controller.js
@@ -1,9 +1,6 @@
 (function (exports) {
   function NoteController(noteList) {
     this.noteList = new NoteList();
-    this.noteList.addNote('Beer n Wine n BBQ n sun n fun');
-    this.noteList.addNote('Beer BBQ');
-
 
 // Creates a note list view, passing in the note list model.
     this.noteListView = new NoteListView(this.noteList);
@@ -18,34 +15,3 @@
   exports.NoteController = NoteController;
 })(this);
 
-
-// Add code that gets hold of the app div element in your index.html page.
-window.addEventListener(`DOMContentLoaded`, function (event) {
-  var noteList = new NoteList();
-  var controller = new NoteController(noteList);
-  document.getElementById(
-    'app'
-  ).innerHTML = controller.noteListView.displayView();
-
-
-// listen for hashchange events
-// change content of app element to be HTML for that note
-  window.addEventListener('hashchange', function () {
-    var id = window.location.hash.slice('#')[6];
-    // getNote is method in NoteList to get notes from array
-    var object = controller.noteList.getNotes()[id];
-
-    var singleNoteView = new SingleNote(object);
-    // singleNoteView now basically had everything in it that we need
-    // getHtml is method that renders single note HTML without shortening
-    document.getElementById('app').innerHTML = singleNoteView.getHtml();
-  });
-
-
-// stops the form being submitted when a submit event is triggered.
-  document.getElementById('text').addEventListener('submit', function(event){
-  event.preventDefault();
-  console.log(document.getElementById('text').elements[0].value)
-  })
-
-});
